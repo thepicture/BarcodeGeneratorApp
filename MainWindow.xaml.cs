f9e2abdda75f9f6f9830a5155afd7d633887a8fc
thepicture <85333076+thepicture@users.noreply.github.com>
2022-09-07 20:02:40 +0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using DevExpress.BarCodes;
+using System;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace BarcodeGeneratorApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateBarCode()
+        {
+            try
+            {
+                if (ComboSymbologies.SelectedItem == null || string.IsNullOrWhiteSpace(BarcodeBox.Text))
+                {
+                    return;
+                }
+                BarCode barCode = new BarCode
+                {
+                    CodeText = BarcodeBox.Text.ToUpper(),
+                    Symbology = (Symbology)ComboSymbologies.SelectedItem
+                };
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    barCode.BarCodeImage.Save(stream, ImageFormat.Png);
+                    BitmapImage image = new BitmapImage();
+                    image.BeginInit();
+                    image.StreamSource = stream;
+                    image.CacheOption = BitmapCacheOption.OnLoad;
+                    image.EndInit();
+                    BarCodeImage.Source = image;
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void OnSymbologyChanged(object sender, SelectionChangedEventArgs e)
+        {
+            GenerateBarCode();
+        }
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            GenerateBarCode();
+        }
+    }
+}
